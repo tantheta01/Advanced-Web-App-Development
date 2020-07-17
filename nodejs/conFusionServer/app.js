3f6703eb8280af44a0db26d5a631c603de89da42
greettanay@gmail.com <tantheta01>
2020-07-17 09:45:48 +0530

    Session

--- nodejs/conFusionServer/app.js
+++ nodejs/conFusionServer/app.js
@@ -12,6 +12,8 @@ const promoRouter = require('./routes/promoRouter');
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 var app = express();
+var session = require("express-session")
+var FileStore = require("session-file-store")(session)
 
 const url = "mongodb://localhost:27017/conFusion"
 
@@ -31,11 +33,22 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-00998'));
+// app.use(cookieParser('12345-67890-00998'));
+
+app.use(session({
+	name : 'session-id',
+	secret : '1234567-9988',
+	saveUninitialized : false,
+	resave : false,
+	store : new FileStore()
+}))
+
 
 function auth (req, res, next) {
 
-	if(!req.signedCookies.user){
+	console.log("req.session")
+
+	if(!req.session.user){
 		var author = req.headers.authorization;
 		if(!author) {
 			var err = new Error("You are not authorized bro!")
@@ -50,18 +63,18 @@ function auth (req, res, next) {
 		console.log(password)
 
 		if(username == 'admin' && password == 'password'){
-			res.cookie("user", "admin", { signed : true })
+			req.session.user = "admin"
 			next();
 		}
 			
 		else {
-			var err = new Err("You are not authorized bro!")
+			var err = new Error("You are not authorized bro!")
 			res.setHeader("WWW-Authenticate", 'Basic')
 			err.status = 401
 			return next(err)
 		}
 	}
-	else if(req.signedCookies.user == 'admin'){
+	else if(req.session.user == 'admin'){
 		next()
 	}
 	else
